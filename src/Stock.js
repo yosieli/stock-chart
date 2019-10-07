80fc35dd41117b0839b60b83698e91efd5dbf71d
feven tsegay <feventsegay6@gmail.com>
2019-10-07 12:23:24 -0500

    stock component created

---
+++ src/Stock.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Stock extends  React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Stock </h1>
+            </div>
+
+        )
+    }
+
+}
+export default Stock
