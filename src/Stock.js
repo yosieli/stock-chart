85bb8c369d8a11bc21ec81fbcaefab1aff6fbadc
feven tsegay <feventsegay6@gmail.com>
2019-10-07 13:31:28 -0500

    done

--- src/Stock.js
+++ src/Stock.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Plot from 'react-plotly.js'
 
 class Stock extends  React.Component {
     state = {
@@ -11,14 +12,49 @@ class Stock extends  React.Component {
     }
 
     fetchStock(){
+        const pointer = this
+        console.log(pointer)
         const API_KEY = 'S55FJQ9A7F4NIHVH'
-        const API_CALL = `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=MSFT&apikey=${API_KEY}`
-        console.log(API_CALL)
-    }
+        //let stockSymbol = 'AMZN'
+        const API_CALL = `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=MSFT&outputsize=compact&apikey=${API_KEY}`
+        let stockChartXValuesFunction = []
+        let stockChartYValuesFunction = []
+
+        // console.log(API_CALL)
+        fetch(API_CALL)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result)
+            for(var key in result['Weekly Adjusted Time Series']){
+
+               stockChartXValuesFunction.push(key)
+               stockChartYValuesFunction.push(result['Weekly Adjusted Time Series'][key]['1. open'])
+            }
+            console.log(stockChartXValuesFunction)
+            pointer.setState({
+                stockChartX:stockChartXValuesFunction,
+                stockChartY:stockChartYValuesFunction
+            })
+        })
+
+        }
+    
     render(){
         return(
             <div>
                 <h1>Stock  Market</h1>
+                <Plot
+                    data={[
+                    {
+                        x: this.state.stockChartX,
+                        y: this.state.stockChartY,
+                        type: 'scatter',
+                        mode: 'lines+points',
+                        marker: {color: 'red'},
+                    },
+                    ]}
+                    layout={ {width: 7200, height: 440, title: 'A Fancy Plot'} }
+                />
             </div>
 
         )
