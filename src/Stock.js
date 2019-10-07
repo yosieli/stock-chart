05e326b96f19961217b4f8b1516a0337e18a4e69
feven tsegay <feventsegay6@gmail.com>
2019-10-07 12:41:35 -0500

    got api data

--- src/Stock.js
+++ src/Stock.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
 class Stock extends  React.Component {
+    state = {
+        stockChartX: [],
+        stockChartY: []
+    }
+
+    componentDidMount(){
+        this.fetchStock()
+    }
+
+    fetchStock(){
+        const API_KEY = 'S55FJQ9A7F4NIHVH'
+        const API_CALL = `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=MSFT&apikey=${API_KEY}`
+        console.log(API_CALL)
+    }
     render(){
         return(
             <div>
-                <h1>Stock </h1>
+                <h1>Stock  Market</h1>
             </div>
 
         )
